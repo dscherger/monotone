2ab64dccb54bfcf55ea11f63a507786ccf19ac83
Christof Petig <christof@petig-baender.de>
2004-06-07 06:24:20 +0000

    PipeStream ...

--- netsync.cc
+++ netsync.cc
@@ -205,7 +205,11 @@ done_marker
 struct PipeStream : Netxx::Stream
 {	Netxx::socket_type fd_write;
 // override write and other methods ...
+  Netxx::signed_size_type write (const void *buffer, Netxx::size_type length);
 // ctors
+  explicit PipeStream (Netxx::socket_type socketfd, Netxx::socket_type writefd, const Netxx::Timeout &timeout=Netxx::Timeout())
+    : Netxx::Stream(socketfd,timeout), fd_write(writefd)
+  {}
 };
 
 struct 
