dff6c731a7fbbf8ccc7f91506a0d5d7522a02432
Christof Petig <christof@petig-baender.de>
2005-01-07 12:05:28 +0000

    result parsing (valid-requests)

--- cvs_client.cc
+++ cvs_client.cc
@@ -12,6 +12,7 @@
 #include <cassert>
 #include <stdexcept>
 #include <set>
+#include <boost/tokenizer.hpp>
 
 class cvs_client
 { int readfd,writefd;
@@ -105,6 +106,40 @@ std::string cvs_client::readline(int fd)
   }
 }
 
+// this was contributed by Marcelo E. Magallon <mmagallo@debian.org>
+// under the GPL to glade-- in July, 2002
+// adapted to work on sets
+
+template <typename Container>
+void
+stringtok (Container &container, std::string const &in,
+           const char * const delimiters = " \t\n")
+{
+    const std::string::size_type len = in.length();
+          std::string::size_type i = 0;
+
+    while ( i < len )
+    {
+        // eat leading whitespace
+        i = in.find_first_not_of (delimiters, i);
+        if (i == std::string::npos)
+            return;   // nothing left but white space
+
+        // find the end of the token
+        std::string::size_type j = in.find_first_of (delimiters, i);
+
+        // push token
+        if (j == std::string::npos) {
+            container.insert (in.substr(i));
+            return;
+        } else
+            container.insert (in.substr(i, j-i));
+
+        // set up for next loop
+        i = j + 1;
+    }
+}
+
 cvs_client::cvs_client(const std::string &host, const std::string &root, 
                     const std::string &user, const std::string &module)
     : readfd(-1), writefd(-1), bytes_read(0), bytes_written(0)
@@ -149,8 +184,17 @@ cvs_client::cvs_client(const std::string &host, const std::string &root,
               "Wrapper-rcsOption M Mbinary E F MT\n");
   writestr(writefd,"valid-requests\n");
   std::string answer=readline(readfd);
-  std::cerr << answer << '\n';
+//  std::cerr << answer << '\n';
   assert(answer.substr(0,15)=="Valid-requests ");
+  stringtok(Valid_requests,answer.substr(15));
+#if 0 // boost does not provide the needed functionality (preserve -)  
+  typedef boost::tokenizer<char_delimiters_separator<char> > tokenizer;
+  tokenizer tok(answer.substr(15));
+  for(tokenizer::iterator i=tok.begin(); i!=tok.end();++i)
+  { Valid_requests.insert(*i);
+    std::cerr << *i << ' ';
+  }
+#endif  
   writestr(writefd,"UseUnchanged\n"); // ???
   ticker();
 //  writestr(writefd,"Directory .\n");
