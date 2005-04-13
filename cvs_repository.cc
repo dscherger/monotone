a6de2ab1f438d4cec5f242130b9462618df2ec7b
Christof Petig <christof@petig-baender.de>
2005-04-13 20:12:23 +0000

    lots of debugging, fixed logic error during revision - edge assignment

--- cvs_repository.cc
+++ cvs_repository.cc
@@ -686,21 +686,43 @@ void cvs_repository::fill_manifests(std::set<cvs_edge>::iterator e)
     { I(!f->second.known_states.empty());
       if (!(*(f->second.known_states.begin()) <= (*e)))
       // the file does not exist yet (first is not below/equal current edge)
+      { W(F("%s before beginning %d/%d+%d\n") % f->first 
+            % f->second.known_states.begin()->since_when
+            % e->time % (e->time2-e->time));
         continue; 
+      }
+#if 0        
       if ((*(f->second.known_states.rend()) < (*e)))
       // the last revision was already processed (it remains this state)
+      { W(F("%s beyond end %d/%d+%d\n") % f->first 
+            % f->second.known_states.rend()->since_when
+            % e->time % (e->time2-e->time));
         continue;
+      }
+#endif
       cvs_manifest::iterator mi=current_manifest.find(f->first);
       if (mi==current_manifest.end()) // the file is currently dead
       { cvs_file_state s=f->second.known_states.end();
+for (cvs_file_state s2=f->second.known_states.begin();
+     s2!=f->second.known_states.end();++s2)
+{ W(F("%s match %d/%d+%d %d %d\n") % f->first 
+    % s2->since_when
+    % e->time % (e->time2-e->time)
+    % ((*s2)<=(*e))
+    % ( next_edge==edges.end() || ((*s2)<(*next_edge)) ));
+}
         // find last revision that fits but does not yet belong to next edge
         // use until end, or above range, or belongs to next edge
         for (cvs_file_state s2=f->second.known_states.begin();
              s2!=f->second.known_states.end() 
              && (*s2)<=(*e)
-             && ( next_edge==edges.end() || !((*s2)<(*next_edge)) );
+             && ( next_edge==edges.end() || ((*s2)<(*next_edge)) );
              ++s2)
+        { W(F("%s matches %d/%d+%d\n") % f->first 
+            % s2->since_when
+            % e->time % (e->time2-e->time));
           s=s2;
+        }
           
         if (s!=f->second.known_states.end() && !s->dead)
         // a matching revision was found
@@ -714,7 +736,7 @@ void cvs_repository::fill_manifests(std::set<cvs_edge>::iterator e)
         ++s;
         if (s!=f->second.known_states.end() 
             && (*s)<=(*e)
-            && ( next_edge==edges.end() || !((*s)<(*next_edge)) ) )
+            && ( next_edge==edges.end() || ((*s)<(*next_edge)) ) )
         { if (s->dead) current_manifest.erase(mi);
           else 
           { mi->second=s;
