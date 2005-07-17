1d307d325566ce14d6d4ab934e5b132c611a2ea3
Christof Petig <christof@petig-baender.de>
2005-07-17 13:27:07 +0000

    exercise the Probe class on pipes

--- netxx_pipe.cc
+++ netxx_pipe.cc
@@ -186,6 +186,12 @@ simple_pipe_test()
   std::string result;
   char buf[1024];
   Netxx::signed_size_type bytes;
+  Netxx::Probe probe;
+  Netxx::Timeout timeout(2L), instant(0,1);
+  probe.clear();
+  probe.add(pipe, Netxx::Probe::ready_read | Netxx::Probe::ready_oobd);
+  Netxx::Probe::result_type res = probe.ready(timeout);
+  L(F("probe %d\n") % res.first);
   do
   { bytes=pipe.read(buf,sizeof buf);
     if (bytes<=0) break;
