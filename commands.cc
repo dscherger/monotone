24a6b79707038476909ebdb09b1448e05a4e5d9e
Petr Baudis <pasky@suse.cz>
2005-09-29 02:53:07 +0000

    Uhm, fix mis-merge part two. Damn parentheses. ;-)

--- commands.cc
+++ commands.cc
@@ -3401,7 +3401,7 @@ CMD(cvs_import, N_("rcs"), N_("CVSROOT"), N_("import all versions in CVS reposit
   if (args.size() != 1)
     throw usage(name);
 
-  import_cvs_repo(system_path(idx(args, 0))(), app);
+  import_cvs_repo(system_path(idx(args, 0)()), app);
 }
 
 CMD(git, N_("git"),
