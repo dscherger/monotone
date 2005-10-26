bf51b8742e7760173f2aad38fee77538bfff0526
zw <zw@robotika.cz>
2005-10-26 16:37:07 +0000

    monotone.py: use automate stdio wherever possible

--- monotone.py
+++ monotone.py
@@ -1,4 +1,5 @@
 import subprocess
+import threading
 import os.path
 import re
 
@@ -36,6 +37,10 @@ class Monotone:
     def __init__(self, db, executable="monotone"):
         self.db = db
         self.executable = executable
+        self.process = None
+
+    def __del__(self):
+        self.ensure_stopped()
 
     def init_db(self):
         self.run_monotone(["db", "init"])
@@ -48,18 +53,17 @@ class Monotone:
             self.init_db()
 
     def revisions_list(self):
-        output = self.run_monotone(["automate", "select", "i:"])
+        output = self.automate("select", "i:")
         return output.split()
     
     def toposort(self, revisions):
-        output = self.run_monotone(["automate", "toposort", "-@-"],
-                                   "\n".join(revisions) + "\n")
+        output = self.automate("toposort", *revisions)
         sorted = output.split()
         assert len(sorted) == len(revisions)
         return sorted
 
     def get_revision(self, rid):
-        return self.run_monotone(["automate", "get_revision", rid])
+        return self.automate("get_revision", rid)
 
     def get_pubkey_packet(self, keyid):
         return self.run_monotone(["pubkey", keyid])
@@ -92,7 +96,7 @@ class Monotone:
         return packets
 
     def key_names(self):
-        output = self.run_monotone(["automate", "keys"])
+        output = self.automate("keys")
         print output
         keys_parsed = self.basic_io_parser(output)
         ids = {}
@@ -105,6 +109,7 @@ class Monotone:
 
     # returns output as a string, raises an error on error
     def run_monotone(self, args, input=None):
+        self.ensure_stopped()
         process = subprocess.Popen([self.executable, "--db", self.db] + args,
                                    stdin=subprocess.PIPE,
                                    stdout=subprocess.PIPE,
@@ -113,6 +118,47 @@ class Monotone:
         if process.returncode:
             raise MonotoneError, stderr
         return stdout
+    
+    def ensure_running(self):
+        if not self.process:
+            self.process = subprocess.Popen([self.executable, "--db", self.db, "automate", "stdio"],
+                                   stdin=subprocess.PIPE,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+    
+    def ensure_stopped(self):
+        if self.process:    
+            self.process.stdin.close()
+            self.process.wait()
+            self.process = None
+    
+    def automate(self, *args):
+        def formater(args):
+            length_prefixed_args = [str(len(command)) + ":" + command for command in args]
+            return "l" + "".join(length_prefixed_args) + "e"
+
+        def parser(pipe):
+            def get_fields():
+                for i in range(0,4):
+                    field = ""
+                    sep = pipe.read(1)
+                    while not sep == ":":
+                        field += sep
+                        sep = pipe.read(1)
+                    yield field
+            
+            data = "" 
+            while 1:
+                cmd, status, cont, size = get_fields()
+                data += pipe.read(int(size))
+                if cont != "m": break
+            return data
+            
+        self.ensure_running()
+        stdin_write = threading.Thread(target=self.process.stdin.write, args=[formater(args)])
+        stdin_write.setDaemon(True)
+        stdin_write.start()
+        return parser(self.process.stdout)
 
     # feeds stuff into 'monotone read'
     def feeder(self):
