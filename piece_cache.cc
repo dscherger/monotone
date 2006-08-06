3ae4c8a4c4cdba11749cbcd252b145029938d7e4
Nathaniel Smith <njs@pobox.com>
2006-08-06 05:05:15 +0000

    hack hack

--- piece_cache.cc
+++ piece_cache.cc
@@ -29,7 +29,7 @@ namespace
     data const dat;
     static shared_ptr<text_t> create(file_id const & me, file_id const & base,
                                      data const & dat);
-    text_t::~text_t();
+    ~text_t();
     size_t memory_size();
   private:
     text_t(file_id const & me, file_id const & base, data const & dat)
@@ -49,6 +49,20 @@ namespace
     size_t const precursors_memory_size;
 
     void clear() { extents.clear() }
+
+    size_t memory_size()
+    {
+      return (extents.capacity() * sizeof(extent_t)) + sizeof(script_t);
+    }
+    size_t total_memory_size()
+    {
+      return precursors_memory_size + memory_size();
+    }
+
+    static shared_ptr<size_t> create(std::vector<extent_t> const & extents,
+                                     size_t precursors_memory_size);
+    ~script_t();
+  private:
     script_t(std::vector<extent_t> const & extents,
              size_t precursors_memory_size)
       // attempt to convince our vector to allocate exactly the right amount
@@ -57,14 +71,6 @@ namespace
       : extents(extents.begin(), extents.end()),
         precursors_memory_size(precursors_memory_size)
     {}
-    size_t memory_size()
-    {
-      return (extents.size() * sizeof(extent_t));
-    }
-    size_t total_memory_size()
-    {
-      return precursors_memory_size + memory_size();
-    }
   };
 
   // for now, we support only a single global cache
@@ -77,9 +83,10 @@ namespace
   size_t
   text_t::memory_size()
   {
-    // FIXME: should we count the size of the text_t and file_id objects
-    // here too?  and the shared_ptr counter?
-    return this->dat().size();
+    return this->dat().size()
+      + this->me.inner()().size()
+      + this->base.inner()().size()
+      + sizeof(text_t);
   }
 
   shared_ptr<text_t>
@@ -87,7 +94,7 @@ namespace
   {
     shared_ptr<text_t> t = shared_ptr<text_t>(new text_t(me, base, dat));
 
-    total_cached_size += this->memory_size();
+    total_cached_size += t->memory_size();
     safe_insert(cached_texts, std::make_pair(me, weak_ptr<text_t>(t)));
 
     return t;
@@ -97,6 +104,24 @@ namespace
   {
     total_cached_size -= this->memory_size();
     safe_remove(cached_texts, me);
+  }
+
+  shared_ptr<script_t>
+  script_t::create(file_id const & fid,
+                   std::vector<extent_t> const & extents,
+                   size_t precursors_memory_size)
+  {
+    shared_ptr<script_t> s = shared_ptr<script_t>(new script_t(extents,
+                                                               precursors_memory_size));
+    total_cached_size += s->memory_size();
+    script_cache.insert(fid, s);
+
+    return s;
+  }
+
+  script_t::~script_t()
+  {
+    total_cached_size -= this->memory_size();
   }
 
 
@@ -159,13 +184,17 @@ namespace
     // consider whether to fix this code or make this assumption be true
     typedef std::vector<shared_ptr<text_t> > texts_t;
     texts_t texts;
-    file_id curr = fid;
-    while (!(null_id(curr) || script_cache.exists(curr)))
-      {
-        shared_ptr<text_t> text = load_text(curr, db);
-        curr = text->base;
-        texts.append(text);
-      }
+    {
+      std::set<file_id> seen;
+      file_id curr = fid;
+      while (!(null_id(curr) || script_cache.exists(curr)))
+        {
+          safe_insert(seen, curr);
+          shared_ptr<text_t> text = swap_in_text(curr, db);
+          curr = text->base;
+          texts.append(text);
+        }
+    }
     // now texts has our final delta at index 0, and the first delta that can
     // be constructed without further disk operations at the end
     for (texts_t::reverse_iterator i = texts.rbegin(); i != texts.rend(); ++i)
@@ -173,7 +202,7 @@ namespace
         // i->base is already loaded into the cache
         // we need to build a script for *i, and put this script into the
         // cache
-        script_cache.insert(i->me, parse_script(*i));
+        parse_script(*i);
       }
 
     shared_ptr<script_t> script = script_cache.fetch(fid);
@@ -208,11 +237,12 @@ namespace
         
         precursors_memory_size += base_script->total_memory_size();
       }
-    return shared_ptr<script_t>(new script_t(extents, precursors_memory_size));
+    return script_t::create(text->me, extents, precursors_memory_size);
   }
 
 
   // TODO:
-  //   -- make script_t use a create() call like text_t and do accounting
-  //      similarly
-  //   -- more functions
+  //   -- a cheap, public, write-to-db function
+  //   -- a public fetch-from-db function
+  //   -- implement the lru
+  //   -- implement the database interface
