c2846bb502d8c3f09d1cd5eb8f59605834d3187e
Markus Wanner <markus@bluegap.ch>
2007-03-09 19:01:46 +0000

    * Converted the importing_cvs_dryrun to the new format, including   writing of makerepo.sh.

--- tests/importing_cvs_dryrun/__driver__.lua
+++ tests/importing_cvs_dryrun/__driver__.lua
@@ -1,45 +1,26 @@
 
-include("/common/cvs.lua")
 mtn_setup()
 
-writefile("importme.3", "version 3 of test file")
+-- See makerepo.sh on how this repository was created.
+check(get("cvs-repository"))
 
-
--- build the cvs repository
-
-cvs_setup()
-
--- check out the workspace and make some commits
--- note that this has to use copy, rather than rename, to update
--- the file in cvs. Apparently, cvs uses timestamps or something to track
--- file modifications.
-check(cvs("co", "."), 0, false, false)
-mkdir("testdir")
-writefile("testdir/importme", "version 0 of test file")
-tsha0 = sha1("testdir/importme")
-check(cvs("add", "testdir"), 0, false, false)
-check(cvs("add", "testdir/importme"), 0, false, false)
-check(cvs("commit", "-m", 'commit 0', "testdir/importme"), 0, false, false)
-writefile("testdir/importme", "version 1 of test file")
-tsha1 = sha1("testdir/importme")
-check(cvs("commit", "-m", 'commit 1', "testdir/importme"), 0, false, false)
-writefile("testdir/importme", "version 2 of test file")
-tsha2 = sha1("testdir/importme")
-check(cvs("commit", "-m", 'commit 2', "testdir/importme"), 0, false, false)
-writefile("testdir/importme", "version 3 of test file")
-tsha3 = sha1("testdir/importme")
-check(cvs("commit", "-m", 'commit 3', "testdir/importme"), 0, false, false)
+writefile("foo.0", "version 0 of test file foo\n")
+writefile("foo.1", "version 1 of test file foo\n")
+writefile("foo.2", "version 2 of test file foo\n")
+writefile("foo.3", "version 3 of test file foo\n")
+tsha0 = sha1("foo.0")
+tsha1 = sha1("foo.1")
+tsha2 = sha1("foo.2")
+tsha3 = sha1("foo.3")
 
 -- import into monotone using dryrun and check if the database has really
 -- not been touched
 
-check(mtn("--dry-run", "--branch=testbranch", "cvs_import", cvsroot .. "/testdir"), 0, false, false)
+check(mtn("--dry-run", "--branch=testbranch", "cvs_import", "cvs-repository/test"), 0, false, false)
 check(mtn("automate", "get_file", tsha0), 1, false, false)
 check(mtn("automate", "get_file", tsha1), 1, false, false)
 check(mtn("automate", "get_file", tsha2), 1, false, false)
 check(mtn("automate", "get_file", tsha3), 1, false, false)
 
--- also check that history is okay -- has a unique head, and it's the
--- right one.
-
+-- try to check out the testbranch, which should fail, too.
 check(mtn("checkout", "--branch=testbranch", "mtcodir"), 1, false, false)
