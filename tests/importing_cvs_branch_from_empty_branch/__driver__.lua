7ba0de92e3aab91bc7676a6d25d53142b51d2794
Markus Wanner <markus@bluegap.ch>
2007-03-10 12:29:03 +0000

    * tests/importing_cvs_branch_from_empty_branch/*: removed the   dependency on cvs from this test by adding a cvs-repository.   For reference, makerepo.sh has been added.

--- tests/importing_cvs_branch_from_empty_branch/__driver__.lua
+++ tests/importing_cvs_branch_from_empty_branch/__driver__.lua
@@ -1,7 +1,9 @@
 
-include("/common/cvs.lua")
 mtn_setup()
-cvs_setup()
+
+
+-- See makerepo.sh on how this repository was created.
+check(get("cvs-repository"))
 
 -- A test case from Michael Haggerty, author of cvs2svn
 -- http://cvs2svn.tigris.org/servlets/ReadMsg?list=dev&msgNo=1878
@@ -10,62 +12,23 @@ cvs_setup()
 -- adoption for monotone automatically checks the result, but is
 -- otherwise equivalent.
 
-check(cvs("co", "."), 0, false, false)
-mkdir("testdir")
-check(cvs("add", "testdir"), 0, false, false)
-check(cvs("commit", "-m", "add dir"), 0, false, false)
-
-writefile("testdir/fileA", "1.1")
-check(cvs("add", "testdir/fileA"), 0, false, false)
-
-writefile("testdir/fileB", "1.1")
-check(cvs("add", "testdir/fileB"), 0, false, false)
-
-writefile("testdir/fileC", "1.1")
-check(cvs("add", "testdir/fileC"), 0, false, false)
-
-writefile("testdir/fileD", "1.1")
-check(cvs("add", "testdir/fileD"), 0, false, false)
-
-check(cvs("commit", "-m", "initial versions"), 0, false, false)
-
-check(cvs("tag", "-b", "A1"), 0, false, false)
-
-writefile("testdir/fileD", "1.2")
-check(cvs("commit", "-m", "revision 1.2 of fileD"), 0, false, false)
-
-check(cvs("tag", "-b", "A2"), 0, false, false)
-
--- switch to branch "A1"
-check(cvs("update", "-r", "A1"), 0, false, false)
-
-writefile("testdir/fileB", "1.1.2.1")
-check(cvs("commit", "-m", "revision 1.1.2.1 of fileB"), 0, false, false)
-
--- create branch "B" and switch to branch:
-check(cvs("tag", "-b", "B"), 0, false, false)
-check(cvs("update", "-r", "B"), 0, false, false)
-
-writefile("testdir/fileC", "1.1.6.1")
-check(cvs("commit", "-m", "revision 1.1.6.1 of fileC"), 0, false, false)
-
-check(cvs("tag", "X"), 0, false, false)
 
 -- import and check
-check(mtn("--branch=test", "cvs_import", cvsroot.."/testdir"), 0, false, false)
+check(mtn("--branch=test", "cvs_import", "cvs-repository/test"), 0, false, false)
 
 check(mtn("list", "branches"), 0, true, false)
-check(samelines("stdout", {"test", "test.A1", "test.B"}))
+check(samelines("stdout", {"test", "test.A1", "test.A2", "test.B"}))
 
 check(mtn("checkout", "-b", "test.A1", "mtnco"), 0, false, false)
 check(indir("mtnco", mtn("list", "known")), 0, true, false)
 check(samelines("stdout", {"fileA", "fileB", "fileC", "fileD"}))
 remove("mtnco")
 
+-- checkout tag X and check its contents
 check(mtn("checkout", "-r", "X", "mtnco"), 0, false, false)
 check(indir("mtnco", mtn("list", "known")), 0, true, false)
 check(samelines("stdout", {"fileA", "fileB", "fileC", "fileD"}))
-check(samefile("mtnco/fileA", "testdir/fileA"))
-check(samefile("mtnco/fileB", "testdir/fileB"))
-check(samefile("mtnco/fileC", "testdir/fileC"))
-check(samefile("mtnco/fileD", "testdir/fileD"))
+check(samelines("mtnco/fileA", {"1.1"}))
+check(samelines("mtnco/fileB", {"1.1.2.1"}))
+check(samelines("mtnco/fileC", {"1.1.6.1"}))
+check(samelines("mtnco/fileD", {"1.1"}))
