7e0e81bf8e4554e25bbf17c0455d207796c3a73a
Markus Wanner <markus@bluegap.ch>
2007-03-10 13:09:15 +0000

    * tests/importing_cvs_with_tags_after_delete/*: removed the   dependency on cvs from this test by adding a cvs-repository.   For reference, makerepo.sh has been added.

--- tests/importing_cvs_with_tags_after_delete/__driver__.lua
+++ tests/importing_cvs_with_tags_after_delete/__driver__.lua
@@ -1,7 +1,8 @@
 
-include("/common/cvs.lua")
 mtn_setup()
-cvs_setup()
+
+-- See makerepo.sh on how this repository was created.
+check(get("cvs-repository"))
 
 -- A test case inspired by Marko Macek from the dev@cvs2svn mailing list:
 -- http://cvs2svn.tigris.org/servlets/ReadMsg?list=dev&msgNo=1877
@@ -10,25 +11,8 @@ cvs_setup()
 -- again. The monotone repository is then checked for correct files
 -- in the tagged revisions.
 
-check(cvs("co", "."), 0, false, false)
-mkdir("testdir")
-writefile("testdir/foo", "foo")
-writefile("testdir/bar", "bar")
-check(cvs("add", "testdir"), 0, false, false)
-check(cvs("add", "testdir/foo"), 0, false, false)
-check(cvs("add", "testdir/bar"), 0, false, false)
-check(cvs("commit", "-m", 'Initial import'), 0, false, false)
-
--- check out the repository created and tag it
-check(cvs("tag", "FOO_AND_BAR"), 0, false, false)
-
--- delete bar and tag again
-check(indir("testdir", cvs("remove", "-f", "bar")), 0, false, false)
-check(cvs("commit", "-m", "removed bar"), 0, false, false)
-check(cvs("tag", "FOO_ONLY"), 0, false, false)
-
 -- import and check
-check(mtn("--branch=test", "cvs_import", cvsroot.."/testdir"), 0, false, false)
+check(mtn("--branch=test", "cvs_import", "cvs-repository/test"), 0, false, false)
 
 check(mtn("checkout", "--revision=FOO_AND_BAR", "mtnco"), 0, false, false)
 check(indir("mtnco", mtn("list", "known")), 0, true, false)
@@ -38,4 +22,4 @@ remove("mtnco")
 check(mtn("checkout", "--revision=FOO_ONLY", "mtnco"), 0, false, false)
 check(indir("mtnco", mtn("list", "known")), 0, true, false)
 check(samelines("stdout", {"foo"}))
-remove("mtnco")
+
