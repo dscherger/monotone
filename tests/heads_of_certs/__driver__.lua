ee65892bfd54684ebd55034f34dd861ac9864157
Richard Levitte <richard@levitte.org>
2007-07-13 06:30:31 +0000

    * tests/heads_of_certs/__driver__.lua: Because 'log' changed, let's 	take a look at the actual expansion log lines.

--- tests/heads_of_certs/__driver__.lua
+++ tests/heads_of_certs/__driver__.lua
@@ -16,14 +16,14 @@ check(mtn("cert", first, "testcert", "value=with=equal=signs"))
 check(mtn("cert", second, "testcert", "value"))
 
 -- Check that a log without H: gives both the first and second commit...
-check(mtn("log", "--from=c:testcert", "--last=1"), 0, true, false)
-check(qgrep("Revision: "..first, "stdout"))
-check(qgrep("Revision: "..second, "stdout"))
+check(mtn("log", "--from=c:testcert"), 0, true, true)
+check(qgrep("expanded to '"..first, "stderr"))
+check(qgrep("expanded to '"..second, "stderr"))
 
 -- Check that a log with H: gives only the second commit...
-check(mtn("log", "--from=H:c:testcert", "--last=1"), 0, true, false)
-check(not qgrep("Revision: "..first, "stdout"))
-check(qgrep("Revision: "..second, "stdout"))
+check(mtn("log", "--from=H:c:testcert"), 0, true, true)
+check(not qgrep("expanded to '"..first, "stderr"))
+check(qgrep("expanded to '"..second, "stderr"))
 -- Note that if the third revision is in the log, something else is wrong...
 -- There is really no case for this last test, just paranoia.
-check(not qgrep(third, "stdout"))
+check(not qgrep(third, "stderr"))
