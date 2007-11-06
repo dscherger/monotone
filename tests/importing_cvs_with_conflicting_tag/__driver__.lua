64c1660efb70fc2a1de1d6d16e13e52ff11fff6a
Markus Wanner <markus@bluegap.ch>
2007-11-06 15:15:29 +0000

    * Removed the xfail and expect both conflicting tag tests to succeed   from now on. * Corrected the second one.

--- tests/importing_cvs_with_conflicting_tag/__driver__.lua
+++ tests/importing_cvs_with_conflicting_tag/__driver__.lua
@@ -31,7 +31,7 @@ check(indir("foo.bar", mtn("update", "-r", "ANOTHER_TAG")), 0, false, false)
 check(indir("foo.bar", mtn("list", "known")), 0, true)
 check(samelines("stdout", {"testsrc", "testsrc/fileA", "testsrc/fileB"}))
 check(samelines("foo.bar/testsrc/fileA", {"Version 0 of fileA."}))
-xfail(samelines("foo.bar/testsrc/fileB", {"Version 1 of fileB."}))
+check(samelines("foo.bar/testsrc/fileB", {"Version 1 of fileB."}))
 
 -- Currently, ANOTHER_TAG gets split correctly, but then we simply override
 -- ANOTHER_TAG as soon as the second blob with the same tag is consumed.
