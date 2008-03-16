10e1a98f5bf9f7f28ab6d3d03691a2d42ff667f9
Christof Petig <christof@arcor.de>
2008-03-16 18:32:43 +0000

    adapt to new environment

--- mtn_cvs/options_list.hh
+++ mtn_cvs/options_list.hh
@@ -8,26 +8,26 @@
 
 OPTSET(globals)
 
-OPTVAR(globals, std::vector<utf8>, args, )
+OPTVAR(globals, args_vector, args, )
 OPTION(globals, positionals, true, "--", "")
 #ifdef option_bodies
 {
-  args.push_back(utf8(arg));
+  args.push_back(arg_type(arg));
 }
 #endif
 
-OPTVAR(branch, utf8, branch_name, )
+OPTVAR(branch, branch_name, branchname, )
 OPTION(branch, branch, true, "branch,b", N_("select branch cert for operation"))
 #ifdef option_bodies
 {
-  branch_name = utf8(arg);
+  branchname = branch_name(arg);
 }
 #endif
 
-OPT(since, "since", utf8, , N_("set history start for CVS pull"))
+OPT(since, "since", std::string, , N_("set history start for CVS pull"))
 #ifdef option_bodies
 {
-  since = utf8(arg);
+  since = std::string(arg);
 }
 #endif
 
@@ -83,7 +83,7 @@ GOPT(quiet, "quiet", bool, false,
 {
   quiet = true;
   global_sanity.set_quiet();
-  ui.set_tick_writer(new tick_write_nothing);
+  ui.set_tick_write_nothing();
 }
 #endif
 
@@ -93,7 +93,7 @@ gettext_noop("suppress warning, verbose, informational and progress messages"))
 {
   reallyquiet = true;
   global_sanity.set_reallyquiet();
-  ui.set_tick_writer(new tick_write_nothing);
+  ui.set_tick_write_nothing();
 }
 #endif
 
@@ -102,36 +102,36 @@ GOPT(ticker, "ticker", std::string, ,
 #ifdef option_bodies
 {
   ticker = arg;
-  if (ticker == "none" || global_sanity.quiet)
-    ui.set_tick_writer(new tick_write_nothing);
+  if (ticker == "none" || global_sanity.quiet_p())
+    ui.set_tick_write_nothing();
   else if (ticker == "dot")
-    ui.set_tick_writer(new tick_write_dot);
+    ui.set_tick_write_dot();
   else if (ticker == "count")
-    ui.set_tick_writer(new tick_write_count);
+    ui.set_tick_write_count();
   else
     throw bad_arg_internal(F("argument must be 'none', 'dot', or 'count'").str());
 }
 #endif
 
-GOPT(mtn_binary, "mtn", utf8, , gettext_noop("monotone binary name"))
+GOPT(mtn_binary, "mtn", std::string, , gettext_noop("monotone binary name"))
 #ifdef option_bodies
 {
-  mtn_binary = utf8(arg);
+  mtn_binary = std::string(arg);
 }
 #endif
 
-GOPT(domain, "domain", utf8, "cvs", gettext_noop("synchronization domain"))
+GOPT(domain, "domain", std::string, "cvs", gettext_noop("synchronization domain"))
 #ifdef option_bodies
 {
-  domain = utf8(arg);
+  domain = std::string(arg);
 }
 #endif
 
-OPTVAR(globals, std::vector<utf8>, mtn_options, )
+OPTVAR(globals, std::vector<std::string>, mtn_options, )
 OPTION(globals, mtn_option, true, "mtn-option", N_("pass option to monotone"))
 #ifdef option_bodies
 {
-  mtn_options.push_back(utf8(arg));
+  mtn_options.push_back(std::string(arg));
 }
 #endif
 
@@ -139,7 +139,7 @@ OPTION(globals, dump, true, "dump",
         gettext_noop("file to dump debugging log to, on failure"))
 #ifdef option_bodies
 {
-  global_sanity.filename = system_path(arg).as_external();
+  global_sanity.set_dump_path(system_path(arg).as_external());
 }
 #endif
 
@@ -148,7 +148,7 @@ OPTION(globals, dump, true, "dump",
 
 #ifdef option_bodies
 #define TRANSOPT3(name,optstring,desc) TRANSOPT_sub(name,true,optstring,desc) \
-{ mtn_options.push_back(utf8("--" #name "=" +arg)); }
+{ mtn_options.push_back(std::string("--" #name "=" +arg)); }
 #else
 #define TRANSOPT3(name,optstring,desc) TRANSOPT_sub(name,true,optstring,desc) 
 #endif
@@ -157,7 +157,7 @@ OPTION(globals, dump, true, "dump",
 
 #ifdef option_bodies
 #define TRANSOPT_BOOL(name,desc) TRANSOPT_sub(name,false,#name,desc) \
-{ mtn_options.push_back(utf8("--" #name)); }
+{ mtn_options.push_back(std::string("--" #name)); }
 #else
 #define TRANSOPT_BOOL(name,desc) TRANSOPT_sub(name,false,#name,desc) 
 #endif
