daa5c2547f468f33194b40c4443e24f3595d65a4
Markus Wanner <markus@bluegap.ch>
2008-08-01 19:10:37 +0000

    * Added yet another, carefully handcrafted test repository, which    triggers some logic in cycle_splitter which still needs an    implementation.

---
+++ tests/importing_cvs_cycle_splitter5/__driver__.lua
@@ -0,0 +1,15 @@
+
+mtn_setup()
+
+check(get("cvs-repository"))
+
+-- A carefully handcrafted CVS repository, exercising the cycle splitter: We
+-- have three files and some pretty nasty cyclic dependencies:
+--
+--    fileA:  A -> B -> C
+--    file2:       B -> C -> A
+--    file3:            C -> A -> B
+
+-- import into monotone and check presence of files
+xfail(mtn("--branch=test", "cvs_import", "--debug", "cvs-repository/test"), 0, false, false)
+
