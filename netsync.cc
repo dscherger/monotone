460b5f6d24258becdb5a95a78ecc4151879687bd
Timothy Brownawell <tbrownaw@gmail.com>
2009-02-19 03:08:50 +0000

    Add a timeout check to maybe_step, so it won't spin for too long.

--- netsync.cc
+++ netsync.cc
@@ -2581,11 +2581,19 @@ session::begin_service()
 void
 session::maybe_step()
 {
+  date_t start_time = date_t::now();
+
   while (done_all_refinements()
          && !rev_enumerator.done()
          && !output_overfull())
     {
       rev_enumerator.step();
+
+      // Safety check, don't spin too long without
+      // returning to the event loop.
+      s64 elapsed_millisec = date_t::now() - start_time;
+      if (elapsed_millisec > 1000 * 10)
+        break;
     }
 }
 
