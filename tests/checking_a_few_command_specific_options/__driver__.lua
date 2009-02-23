ded2de789a9e74e78fed4d976d6d8eec82818a31
Derek Scherger <derek@echologic.com>
2009-02-23 03:41:22 +0000

    fix tests failing due to new log options
    * tests/checking_a_few_command_specific_options/__driver__.lua: replace
      --branch, which is now a valid option for log, with --full which is not; this
      test relies on the log command failing on a bad option
    * tests/log_--brief/__driver__.lua: add --branch=* to log so that all branches are listed
      as this test expects
    
    more info on suspension here: http://article.gmane.org/gmane.comp.version-control.monotone.devel/16146

--- tests/checking_a_few_command_specific_options/__driver__.lua
+++ tests/checking_a_few_command_specific_options/__driver__.lua
@@ -6,7 +6,7 @@ check(mtn("--branch=testbranch", "add", "testfile"), 1, false, false)
 check(mtn("add", "testfile"), 0, false, false)
 commit()
 
-check(mtn("--branch=testbranch", "--last=1", "log"), 1, false, false)
+check(mtn("--full", "--last=1", "log"), 1, false, false)
 check(mtn("--last=1", "log"), 0, false, false)
 
 check(mtn("--depth=0", "status"), 0, false, false)
