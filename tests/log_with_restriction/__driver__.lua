9da4b8845720c3ef252a0e697616e83bfb1b2f5b
Derek Scherger <derek@echologic.com>
2009-02-26 06:30:49 +0000

    add missing --depth and --exclude options to log and fix associated problems
    * NEWS: add note about log now supporting --depth and --exclude options
    
    * cmd_diff_log.cc (log): rewrap options declarations so that lines end with '|'
      rather than starting with them; add missing --depth and --exclude options;
      check for exclude patterns and initialize the restriction when specified;
      remove extraneous braces around fetch for relatives; simplify checks for next
      and last in initialization of use_markings and stop condition; flush cout
      after printing a revision so that output arrives more smoothly
    
    * tests/log_--last=N_--next=N/__driver__.lua:
    * tests/log_--last=N_FILENAME/__driver__.lua:
    * tests/log_dir/__driver__.lua: add --no-graph to log invocations and simplify
      regexes that no longer need to worry about leading \|/
    
    * tests/log_with_restriction/__driver__.lua: add tests for log with --depth and
      --exclude options; add --no-graph to log invocations and simplify regexes that
      no longer need to worry about leading \|/

--- tests/log_with_restriction/__driver__.lua
+++ tests/log_with_restriction/__driver__.lua
@@ -3,6 +3,76 @@
 
 mtn_setup()
 
+mkdir("1")
+mkdir("1/2")
+mkdir("1/2/3")
+mkdir("1/2/3/4")
+
+check(mtn("add", "."), 0, true, false)
+commit("testbranch")
+
+check(mtn("add", "1"), 0, true, false)
+commit("testbranch")
+
+check(mtn("add", "1/2"), 0, true, false)
+commit("testbranch")
+
+check(mtn("add", "1/2/3"), 0, true, false)
+commit("testbranch")
+
+check(mtn("add", "1/2/3/4"), 0, true, false)
+commit("testbranch")
+
+-- the project tree now looks like this
+-- .          root depth=0 added in 1st commit
+-- ./1        dir  depth=1 added in 2nd commit
+-- ./1/2      dir  depth=2 added in 3rd commit
+-- ./1/2/3    dir  depth=3 added in 4th commit
+-- ./1/2/3/4  dir  depth=4 added in 5th commit
+
+-- include commit 1
+check(mtn("log", "--no-graph", "--brief", "--depth", "0", "."), 0, true, false)
+check(numlines("stdout") == 1)
+
+-- include commits 1,2
+check(mtn("log", "--no-graph", "--brief", "--depth", "1", "."), 0, true, false)
+check(numlines("stdout") == 2)
+
+-- include commits 1,2,3
+check(mtn("log", "--no-graph", "--brief", "--depth", "2", "."), 0, true, false)
+check(numlines("stdout") == 3)
+
+-- include commits 1,2,3,4
+check(mtn("log", "--no-graph", "--brief", "--depth", "3", "."), 0, true, false)
+check(numlines("stdout") == 4)
+
+-- include commits 1,2,3,4,5
+check(mtn("log", "--no-graph", "--brief", "--depth", "4", "."), 0, true, false)
+check(numlines("stdout") == 5)
+
+
+-- exclude commit 1
+check(mtn("log", "--no-graph", "--brief", "--depth", "0", "--exclude", "."), 0, true, false)
+check(numlines("stdout") == 4)
+
+-- eclude commits 1,2
+check(mtn("log", "--no-graph", "--brief", "--depth", "1", "--exclude", "."), 0, true, false)
+check(numlines("stdout") == 3)
+
+-- exclude commits 1,2,3
+check(mtn("log", "--no-graph", "--brief", "--depth", "2", "--exclude", "."), 0, true, false)
+check(numlines("stdout") == 2)
+
+-- exclude commits 1,2,3,4
+check(mtn("log", "--no-graph", "--brief", "--depth", "3", "--exclude", "."), 0, true, false)
+check(numlines("stdout") == 1)
+
+-- exclude commits 1,2,3,4,5
+check(mtn("log", "--no-graph", "--brief", "--depth", "4", "--exclude", "."), 0, true, false)
+check(numlines("stdout") == 0)
+
+
+
 addfile("early", "early")
 commit("testbranch", "Addition of an early file.")
 
@@ -25,13 +95,14 @@ writefile("foo", "changed foo again")
 commit("testbranch", "foo has changed again.")
 rev_foo3 = base_revision()
 
-check(mtn("log", "foo"), 0, true, false)
+check(mtn("log", "--no-graph", "foo"), 0, true, false)
 rename("stdout", "log")
 
-check(grep("^[\\|\\\\\/ ]+Revision:", "log"), 0, true, false)
+check(grep("^Revision:", "log"), 0, true, false)
 rename("stdout", "revs")
 check(numlines("revs") == 3)
 
-check(grep("^[\\|\\\\\/ ]+Revision: " .. rev_foo1, "log"), 0, true)
-check(grep("^[\\|\\\\\/ ]+Revision: " .. rev_foo2, "log"), 0, true)
-check(grep("^[\\|\\\\\/ ]+Revision: " .. rev_foo3, "log"), 0, true)
+check(grep("^Revision: " .. rev_foo1, "log"), 0, true)
+check(grep("^Revision: " .. rev_foo2, "log"), 0, true)
+check(grep("^Revision: " .. rev_foo3, "log"), 0, true)
+
