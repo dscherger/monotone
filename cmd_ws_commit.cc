9ce3784f9a49dfd01063fe06ed8d1549c457c8bf
Derek Scherger <derek@echologic.com>
2010-03-06 04:43:25 +0000

    move revision_header and revision_summary functions into their own source file
     * Makefile.am: add rev_output.{cc,hh}
     * cmd_ws_commit.cc (revision_summary, revision_header): move to ...
     * rev_output.{cc,hh} (revision_summary, revision_header): ... here; where they
       are available for log

--- cmd_ws_commit.cc
+++ cmd_ws_commit.cc
@@ -31,6 +31,7 @@
 #include "simplestring_xform.hh"
 #include "database.hh"
 #include "roster.hh"
+#include "rev_output.hh"
 #include "vocab_cast.hh"
 
 using std::cout;
@@ -44,111 +45,6 @@ using std::string;
 using std::vector;
 
 using boost::shared_ptr;
-
-static void
-revision_header(revision_id rid, revision_t const & rev, string const & author, 
-                date_t const date, branch_name const & branch, 
-                bool const branch_changed, utf8 & header)
-{
-  ostringstream out;
-  int const width = 70;
-
-  // FIXME bad suffix
-  out << string(width, '-') << '\n'
-      << _("Revision: ") << rid << _("       (uncommitted)") << '\n';
-
-  for (edge_map::const_iterator i = rev.edges.begin(); i != rev.edges.end(); ++i)
-    {
-      revision_id parent = edge_old_revision(*i);
-      out << _("Parent: ") << parent << '\n';
-    }
-
-  out << _("Author: ") << author << '\n'
-      << _("Date: ") << date << '\n';
-
-  if (branch_changed)
-    {
-      // FIXME bad suffix
-      int space = width - branch().length() - 8 - 10;
-      if (space < 0) space = 0;
-      out << _("Branch: ") << branch << string(space, ' ') << _(" (changed)") << '\n';
-    }
-  else
-    out << _("Branch: ") << branch << '\n';
-
-  out << _("Changelog:") << "\n\n";
-
-  header = utf8(out.str(), origin::internal);
-}
-
-static void
-revision_summary(revision_t const & rev, utf8 & summary)
-{
-  // We intentionally do not collapse the final \n into the format
-  // strings here, for consistency with newline conventions used by most
-  // other format strings.
-
-  ostringstream out;
-  revision_id rid;
-  calculate_ident(rev, rid);
-
-  for (edge_map::const_iterator i = rev.edges.begin(); i != rev.edges.end(); ++i)
-    {
-      revision_id parent = edge_old_revision(*i);
-      cset const & cs = edge_changes(*i);
-
-      out << '\n';
-
-      // A colon at the end of this string looked nicer, but it made
-      // double-click copying from terminals annoying.
-      if (!null_id(parent))
-        out << _("Changes against parent ") << parent << "\n\n";
-
-      // presumably a merge rev could have an empty edge if one side won
-      if (cs.empty())
-        out << _("no changes") << '\n';
-
-      for (set<file_path>::const_iterator i = cs.nodes_deleted.begin();
-            i != cs.nodes_deleted.end(); ++i)
-        out << _("  dropped  ") << *i << '\n';
-
-      for (map<file_path, file_path>::const_iterator
-            i = cs.nodes_renamed.begin();
-            i != cs.nodes_renamed.end(); ++i)
-        out << _("  renamed  ") << i->first
-            << _("       to  ") << i->second << '\n';
-
-      for (set<file_path>::const_iterator i = cs.dirs_added.begin();
-            i != cs.dirs_added.end(); ++i)
-        out << _("  added    ") << *i << '\n';
-
-      for (map<file_path, file_id>::const_iterator i = cs.files_added.begin();
-            i != cs.files_added.end(); ++i)
-        out << _("  added    ") << i->first << '\n';
-
-      for (map<file_path, pair<file_id, file_id> >::const_iterator
-              i = cs.deltas_applied.begin(); i != cs.deltas_applied.end(); ++i)
-        out << _("  patched  ") << i->first << '\n';
-
-      for (map<pair<file_path, attr_key>, attr_value >::const_iterator
-             i = cs.attrs_set.begin(); i != cs.attrs_set.end(); ++i)
-        out << _("  attr on  ") << i->first.first << '\n'
-            << _("      set  ") << i->first.second << '\n'
-            << _("       to  ") << i->second << '\n';
-
-      // FIXME: naming here could not be more inconsistent
-      // the cset calls it attrs_cleared
-      // the command is attr drop
-      // here it is called unset
-      // the revision text uses attr clear 
-
-      for (set<pair<file_path, attr_key> >::const_iterator
-             i = cs.attrs_cleared.begin(); i != cs.attrs_cleared.end(); ++i)
-        out << _("  attr on  ") << i->first << '\n'
-            << _("    unset  ") << i->second << '\n';
-    }
-  summary = utf8(out.str(), origin::internal);
-}
 
 static void
 get_old_branch_names(database & db, parent_map const & parents,
