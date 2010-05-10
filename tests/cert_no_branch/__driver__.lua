d62887cfe7d6f7dd27429ca8553595804c6c49af
Stephen Leake <stephen_leake@stephe-leake.org>
2010-05-10 09:59:46 +0000

    * tests/cert_no_branch/__driver__.lua (rev): also remove branch cert from revision before running "cert"

--- tests/cert_no_branch/__driver__.lua
+++ tests/cert_no_branch/__driver__.lua
@@ -1,13 +1,18 @@
--- test that 'mtn cert' does not require a branch
+-- test that 'mtn cert' does not require a branch option, even when
+-- there is no branch cert on the revision.
+--
 -- bug# 7221
 
 mtn_setup()
 
 addfile("base", "base")
-commit()
+commit("testbranch")
 rev = base_revision()
 
--- ensure we don't find a branch
+-- delete the branch cert from the revision
+check(mtn("db", "kill_branch_certs_locally", "testbranch"), 0, false, false)
+
+-- ensure we don't find a branch option
 remove("_MTN")
 
 check(mtn("cert", rev, "branch", "anotherbranch"), 0, false, false)
