ad1d232e6c3b963b289aa6197aa7c53651972263
Derek Scherger <derek@echologic.com>
2010-05-12 04:53:10 +0000

    add more comments and checks to various diff tests
     * tests/diff_restriction_renamed_parent/__driver__.lua: replace fixme with
       checks for unrestricted and partially restricted diffs which do succeed; the
       diff only fails when restricted to a single file
    
     * tests/diff_shows_renames/__driver__.lua: un-xfail; using rosters as the basis
       of the diffs actually does fixes it as predicted
    
     * tests/diffing_a_revision_with_an_added_file/__driver__.lua: assert that the
       diff has /dev/null as the source and the added file as the target
    
     * tests/log_--diffs/__driver__.lua: don't include the initial rev in the first
       log with diffs because the restriction in the log will cause it to fail; add
       comments and an xfailed test that does attempt to log all revs
    
     * tests/log_--diffs/expect_restrict: remove diff from intial rev in expectation
    
     * tests/restricted_diff_bug/__driver__.lua: replace fixme with comments about
       why this test fails and how it should probably be fixed

--- tests/diff_restriction_renamed_parent/__driver__.lua
+++ tests/diff_restriction_renamed_parent/__driver__.lua
@@ -13,5 +13,20 @@ commit()
 writefile("parent2/file1", "something else")
 commit()
 
--- fixme
+check(mtn("diff", "-r", orig), 0, true, false)
+check(qgrep("^--- parent1/file1\t", "stdout"))
+check(qgrep("^\\+\\+\\+ parent2/file1\t", "stdout"))
+
+check(mtn("diff", "-r", orig, "parent2"), 0, true, false)
+check(qgrep("^--- parent1/file1\t", "stdout"))
+check(qgrep("^\\+\\+\\+ parent2/file1\t", "stdout"))
+
+-- this fails because restricting to parent2/file1 excludes the rename
+-- of parent1 to parent2 and when diff goes to get the file content
+-- for file1 it looks in parent1/file1 rather than parent2/file1
+
+-- the solution here is probably to make the restrictions code implicitly
+-- include the parents, non-recursively, of all explicitly included nodes
+-- then the parent rename would be included here and the diff would work.
+
 xfail(mtn("diff", "-r", orig, "parent2/file1"), 0, true, true)
--- tests/diff_shows_renames/__driver__.lua
+++ tests/diff_shows_renames/__driver__.lua
@@ -11,15 +11,10 @@ check(mtn("diff"), 0, true, false)
 check(qgrep("^--- foo/bar\t", "stdout"))
 check(qgrep("^\\+\\+\\+ foo/newbar\t", "stdout"))
 
--- The bug here is that the diff code tries to infer renames from the csets
--- involved.  It should just use the pre- and post-roster to get the pre- and
--- post-name.
-
 remove("foo")
 revert_to(root_rev)
 check(mtn("mv", "foo", "newfoo"), 0, false, false)
 writefile("newfoo/bar", "version 2!\n")
 check(mtn("diff"), 0, true, false)
--- fixme
-xfail(qgrep("^--- foo/bar\t", "stdout"))
+check(qgrep("^--- foo/bar\t", "stdout"))
 check(qgrep("^\\+\\+\\+ newfoo/bar\t", "stdout"))
