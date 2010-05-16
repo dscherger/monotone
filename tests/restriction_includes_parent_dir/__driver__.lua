c5361e4fdf4ed85a65dd76ca5d7a9a55af9247ee
Derek Scherger <derek@echologic.com>
2010-05-16 21:10:26 +0000

    update most of lua testsuite to work with new restriction semantics; there are a few revert tests that are still failing but implicitly including parents seems like it's not the right thing to do for revert
     * tests/automate_inventory_ignore_dirs/expected.stderr:
     * tests/automate_inventory_ignore_dirs/expected_2.stderr:
       allow for missing parent warnings on stderr
    
     * tests/automate_inventory_ignore_dirs/expected.stdout:
     * tests/automate_inventory_ignore_dirs/expected_2.stdout:
       add root directory included by new implicit inclusion rules
    
     * tests/automate_inventory_options/__driver__.lua:
       add new file for --no-unchanged test
    
     * tests/automate_inventory_options/expected-no-ignored.stdout:
     * tests/automate_inventory_options/expected-no-unknown.stdout:
     * tests/automate_inventory_options/expected-none.stdout:
     * tests/automate_inventory_options/expected-renames-both.stdout:
     * tests/automate_inventory_options/expected-renames-source.stdout:
     * tests/automate_inventory_options/expected-renames-target-no-ignored.stdout:
     * tests/automate_inventory_options/expected-renames-target-no-unknown.stdout:
     * tests/automate_inventory_options/expected-renames-target.stdout:
       add root directory included by new implicit inclusion rules
    
     * tests/automate_inventory_options/expected-renames-target-no-unchanged.stdout:
       new expected output for --no-unchanged test that does not include root
       directory
    
     * tests/automate_inventory_restricted/__driver__.lua:
       add root directory included by new implicit inclusion rules
    
     * tests/diff_against_empty_file/__driver__.lua:
     * tests/diff_on_missing_trailing_newline_at_end_of_file/__driver__.lua:
       allow for missing parent warnings on stderr
    
     * tests/diff_restriction_renamed_parent/__driver__.lua:
       allow for missing parent warnings on stderr; don't keep stdout files;
       un-xfail
    
     * tests/fetching_nonexistent_entry_from_children_bug/__driver__.lua:
       document new behaviour and change the check to expect success rather than
       failure; fixes bug #15994
    
     * tests/log_dir/__driver__.lua:
       add root directory included by new implicit inclusion rules; allow for
       missing parent warnings on stderr
    
    
     * tests/log_with_restriction/__driver__.lua: changes to the root directory will
       always be included in log output so the root revision will not be excluded
    
     * tests/manifest_restrictions/__driver__.lua: remove stale comment and enable
       previously commented out checks
    
     * tests/merge_into_workspace/__driver__.lua:
     * tests/merge_update_part_of_file/__driver__.lua:
       allow for missing parent warnings on stderr
    
     * tests/restricted_diff_bug/__driver__.lua: un-xfail
    
     * tests/restriction_excludes_parent_dir: rename to ...
     * tests/restriction_includes_parent_dir: ... this since the rules have changed
     * tests/restriction_includes_parent_dir/__driver__.lua:
       remove stale comments; change check calls to expect success rather than
       failure; add more checks to command output; add checks for various usages of
       --depth option
    
     * tests/restrictions_with_renames_and_adds/__driver__.lua:
       update tests previously considered "nonsensical" to expect success rather
       than failure
    
     * tests/revert_file_to_base_revision/__driver__.lua:
     * tests/subdirectory_restrictions/__driver__.lua:
       allow for missing parent warnings on stderr
    
     * tests/use_restrictions_with_--exclude/__driver__.lua:
       document, update and enable previously disabled checks

---
+++ tests/restriction_includes_parent_dir/__driver__.lua
@@ -0,0 +1,74 @@
+
+mtn_setup()
+
+-- Restricting a new project to a single file excludes the root directory
+-- addition. The new implicit include semantics cause the root directory to
+-- be included as the required parent of the specified file.
+
+addfile("file", "file")
+
+-- exclude newly added root dir but include file below it
+-- this will implicitly include the root dir
+
+check(mtn("st", "file"), 0, true, true)
+rename("stdout", "additions")
+rename("stderr", "warnings")
+
+check(grep("added", "additions"), 0, true, false)
+check(numlines("stdout") == 2)
+check(grep("including missing parent", "warnings"), 0, true, false)
+check(numlines("stdout") == 1)
+
+commit()
+
+mkdir("foo")
+
+addfile("foo/bar", "foobar")
+
+-- exclude newly added foo dir but include bar below it
+-- this will implicitly include the new foo dir and the root dir
+
+check(mtn("st", "foo/bar"), 0, true, true)
+rename("stdout", "additions")
+rename("stderr", "warnings")
+
+check(grep("added", "additions"), 0, true, false)
+check(numlines("stdout") == 2)
+check(grep("including missing parent", "warnings"), 0, true, false)
+check(numlines("stdout") == 2)
+
+-- ensure that --depth=0 means "directory without contents" rather
+-- than "directory and all immediate children"
+
+check(mtn("st", "--depth", "0"), 0, true, false)
+check(qgrep("no changes", "stdout"))
+
+check(mtn("st", "--depth", "1"), 0, true, false)
+check(qgrep("foo", "stdout"))
+check(not qgrep("foo/bar", "stdout"))
+
+check(mtn("st", "--depth", "2"), 0, true, false)
+check(qgrep("foo", "stdout"))
+check(qgrep("foo/bar", "stdout"))
+
+-- ensure that --depth is relative to the current directory
+
+chdir("foo")
+check(mtn("st", ".", "--depth", "0"), 0, true, false)
+check(qgrep("foo", "stdout"))
+check(not qgrep("foo/bar", "stdout"))
+
+check(mtn("st", ".", "--depth", "1"), 0, true, false)
+check(qgrep("foo", "stdout"))
+check(qgrep("foo/bar", "stdout"))
+
+-- ensure that --depth is relative to the specified directory
+
+chdir("..")
+check(mtn("st", "foo", "--depth", "0"), 0, true, false)
+check(qgrep("foo", "stdout"))
+check(not qgrep("foo/bar", "stdout"))
+
+check(mtn("st", "foo", "--depth", "1"), 0, true, false)
+check(qgrep("foo", "stdout"))
+check(qgrep("foo/bar", "stdout"))
