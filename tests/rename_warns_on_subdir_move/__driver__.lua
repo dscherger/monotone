535505e76a4f0311a83540009e0e758acaf5aea6
Thomas Keller <me@thomaskeller.biz>
2010-05-20 09:07:37 +0000

    Added an partially xfailing test; a rename into a subdirectory of the rename source is still not properly catched.

---
+++ tests/rename_warns_on_subdir_move/__driver__.lua
@@ -0,0 +1,15 @@
+
+mtn_setup()
+addfile("file", "file")
+adddir("dir")
+adddir("dir/subdir")
+commit()
+
+check(mtn("mv", "file", "file"), 1, false, true)
+check(qgrep("destination file/ is not a directory", "stderr"))
+
+check(mtn("mv", "dir", "dir"), 0, false, true)
+check(qgrep("cannot move `dir' to a subdirectory of itself, `dir/dir'", "stderr"))
+
+xfail(mtn("mv", "dir", "dir/subdir"), 0, false, true)
+--check(qgrep("cannot move `dir' to a subdirectory of itself, `dir/subdir/dir'", "stderr"))
