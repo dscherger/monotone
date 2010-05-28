7e7cbbeb819ffd1732baa3b7898d029372e498de
Timothy Brownawell <tbrownaw@prjek.net>
2010-05-28 02:18:52 +0000

    tweak policy-keys test

--- tests/policy-keys/__driver__.lua
+++ tests/policy-keys/__driver__.lua
@@ -57,6 +57,11 @@ check(mtn("create_branch", "test_project.delegated.otherbranch",
 check(mtn("create_branch", "test_project.badbranch",
 	  "--no-workspace", "-k", "other_key"), 1, nil, false)
 
+-- ...unless fully qualified
+check(mtn("create_branch", "test_project.badbranch",
+	  "--no-workspace", "-k", "other_project.other_key"), 0, nil, false)
+
+
 -- drop private key (dropkey -d:memory:)
 check(mtn("-d", ":memory:", "--no-workspace", "dropkey", my_key), 0, nil, false)
 
