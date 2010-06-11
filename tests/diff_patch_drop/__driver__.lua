a470fe5bf759a238c5f8889becede8d99810c7ed
Thomas Keller <me@thomaskeller.biz>
2010-06-11 07:50:55 +0000

    * tests/diff_patch_drop/__driver__.lua: patch from openBSD (and possibly   other BSDs as well) does only drop empty files and directories if   explicitely announced with the -E option

--- tests/diff_patch_drop/__driver__.lua
+++ tests/diff_patch_drop/__driver__.lua
@@ -24,7 +24,15 @@ check(exists("dir/file"))
 -- but it is a little aggressive and removes the dir too!
 
 copy("diff", "stdin")
-check({"patch", "-p0"}, 0, false, false, true)
+
+-- patch from openBSD and possibly other BSDs as well
+-- does not automatically drop empty files / directories, see
+-- http://article.gmane.org/gmane.comp.version-control.monotone.devel/17597
+if string.match(ostype, "BSD") then
+    check({"patch", "-p0", "-E"}, 0, false, false, true)
+else
+    check({"patch", "-p0"}, 0, false, false, true)
+end
 
 check(not exists("dir/file"))
 check(not exists("dir"))
