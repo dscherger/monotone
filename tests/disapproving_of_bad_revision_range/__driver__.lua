da2df242bddb24cabcbbb204edbe8d162c83b893
Tero Koskinen <tero.koskinen@iki.fi>
2010-06-29 19:45:49 +0000

    Use addfile(...) instead of get(rootfile).

--- tests/disapproving_of_bad_revision_range/__driver__.lua
+++ tests/disapproving_of_bad_revision_range/__driver__.lua
@@ -1,10 +1,9 @@
 
 mtn_setup()
 
-check(get("rootfile", "testfile"))
+addfile("testfile","dummy content")
 check(mtn("add", "testfile"), 0, false, false)
 commit()
 root_r_sha = base_revision()
-root_f_sha = sha1("testfile")
 
 check(mtn("disapprove", root_r_sha, "deadcafe"), 1, false, false)
