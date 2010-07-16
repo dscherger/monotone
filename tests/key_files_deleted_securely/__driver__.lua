d56815810879175ca0bb2760543ffec017ebbb84
Thomas Keller <me@thomaskeller.biz>
2010-07-16 23:26:31 +0000

    * key_store.cc (delete_key): if no new-style key file is found, try   to use the old-style file name without the hash; also ensure that   we delete the correct key file by reading in the key for the last   time and compare its ident with the ident of the key to delete   (closes #30376) * tests/key_files_deleted_securely: new test case for both issues * NEWS: note the bugfix

---
+++ tests/key_files_deleted_securely/__driver__.lua
@@ -0,0 +1,25 @@
+mtn_setup()
+
+-- create two keys, rename both, the first to a custom name,
+-- the second to an old-style name without ident
+
+check(mtn("genkey", "foobar"), 0, false, true, "\n\n")
+check(grep("has hash", "stderr"), 0, true)
+line = readfile("stdout")
+keyid1 = string.sub(line, -42, -3)
+check(rename("keys/foobar."..keyid1, "keys/something-else"))
+
+check(mtn("genkey", "foobar", "--force-duplicate-key"), 0, false, true, "\n\n")
+check(grep("has hash", "stderr"), 0, true)
+line = readfile("stdout")
+keyid2 = string.sub(line, -42, -3)
+check(rename("keys/foobar."..keyid2, "keys/foobar"))
+
+check(mtn("dropkey", keyid1), 1, false, true)
+check(qgrep("expected key with id '" ..keyid1.."' in key file", "stderr"))
+
+-- if the conflicting key is gone, ensure that dropkey properly deletes
+-- the old key file, even if it has no keyid suffixed
+remove("keys/something-else")
+check(mtn("dropkey", "foobar"), 0, false, false)
+
