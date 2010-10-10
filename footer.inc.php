c23e75e2fc82bbb46477b854aece80bef2159bef
Thomas Keller <me@thomaskeller.biz>
2010-10-10 22:54:22 +0000

    * ignore downloads and config.inc.php * remove link to _analog.html, which we do not have on the new server

--- footer.inc.php
+++ footer.inc.php
@@ -1,6 +1,6 @@
 
     <div id="footer">
-        <a href="_analog.html">website statistics</a> | <a href="http://tango.freedesktop.org/Tango_Icon_Library">icons stolen from the Tango Project</a>
+        <a href="http://tango.freedesktop.org/Tango_Icon_Library">icons stolen from the Tango Project</a>
     </div>
 </div>
 
