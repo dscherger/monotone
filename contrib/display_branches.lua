da4ed43c026897f30153cca055c953144630107c
Richard Levitte <richard@levitte.org>
2011-01-24 21:42:33 +0000

    Have it display branches (and counts) in both directions.

--- contrib/display_branches.lua
+++ contrib/display_branches.lua
@@ -7,26 +7,31 @@
 do
    netsync_branches = {}
 
-   function RL_note_netsync_cert_received(rev_id,key,name,value,nonce)
+   function RL_note_netsync_cert_received(direction,rev_id,key,name,value,nonce)
       if name == "branch" then
-	 if netsync_branches[nonce][value] == nil then
-	    netsync_branches[nonce][value] = 1
+	 if netsync_branches[direction][nonce][value] == nil then
+	    netsync_branches[direction][nonce][value] = 1
 	 else
-	    netsync_branches[nonce][value] = netsync_branches[nonce][value] + 1
+	    netsync_branches[direction][nonce][value] =
+	       netsync_branches[direction][nonce][value] + 1
 	 end
       end
    end
 
    notifier = {
-      start =
+      ["start"] =
 	 function(session_id,...)
-	    netsync_branches[session_id] = {}
+	    netsync_branches["received"] = {}
+	    netsync_branches["sent"] = {}
+	    netsync_branches["received"][session_id] = {}
+	    netsync_branches["sent"][session_id] = {}
 	    return "continue",nil
 	 end,
-      revision_received =
+      ["revision_received"] =
 	 function(new_id,revision,certs,session_id)
 	    for _, item in pairs(certs) do
-	       RL_note_netsync_cert_received(new_id,
+	       RL_note_netsync_cert_received("received",
+					     new_id,
 					     item.key,
 					     item.name,
 					     item.value,
@@ -34,26 +39,65 @@ do
 	    end
 	    return "continue",nil
 	 end,
-      cert_received =
+      ["revision_sent"] =
+	 function(new_id,revision,certs,session_id)
+	    for _, item in pairs(certs) do
+	       RL_note_netsync_cert_received("sent",
+					     new_id,
+					     item.key,
+					     item.name,
+					     item.value,
+					     session_id)
+	    end
+	    return "continue",nil
+	 end,
+      ["cert_received"] =
 	 function(rev_id,key,name,value,session_id)
-	    RL_note_netsync_cert_received(rev_id,
+	    RL_note_netsync_cert_received("received",
+					  rev_id,
 					  key,name,value,
 					  session_id)
 	    return "continue",nil
 	 end,
-      ["end"] = function(session_id)
-		   local first = true
-		   for item, amount in pairs(netsync_branches[session_id])
-		   do
-		      if first then
-			 io.stderr:write("Affected branches:\n")   
-			 first = false
-		      end
-		      io.stderr:write("  ",item,"  (",amount,")\n")   
-		   end
-		   netsync_branches[session_id] = nil
-		   return "continue",nil
-		end
+      ["cert_sent"] =
+	 function(rev_id,key,name,value,session_id)
+	    RL_note_netsync_cert_received("sent",
+					  rev_id,
+					  key,name,value,
+					  session_id)
+	    return "continue",nil
+	 end,
+      ["end"] =
+	 function(session_id,status)
+	    -- only try to display results if we got
+	    -- at least partial contents
+	    if status > 211 then
+	       return "continue",nil
+	    end
+
+	    local first = true
+	    for item, amount in pairs(netsync_branches["received"][session_id])
+	    do
+	       if first then
+		  io.stderr:write("Received data on branches:\n")
+		  first = false
+	       end
+	       io.stderr:write("  ",item,"  (",amount,")\n")   
+	    end
+	    netsync_branches["received"][session_id] = nil
+
+	    first = true
+	    for item, amount in pairs(netsync_branches["sent"][session_id])
+	    do
+	       if first then
+		  io.stderr:write("Sent data on branches:\n")
+		  first = false
+	       end
+	       io.stderr:write("  ",item,"  (",amount,")\n")   
+	    end
+	    netsync_branches["sent"][session_id] = nil
+	    return "continue",nil
+	 end
    }
 
    local v,m = push_hook_functions(notifier)
