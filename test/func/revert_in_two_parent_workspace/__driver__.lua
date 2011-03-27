fdef5f3a6b22ed0492f06c21281ad339630c6b68
Richard Hopkins <richhguard-monotone@yahoo.co.uk>
2011-03-27 10:06:33 +0000

    Created new revert_in_two_parent_workspace func test
    Moved the existing revert test from two_parent_workspace_commands_that_fail
    into it, and also copied the setup/writefile/commit template to produce a
    workspace with 2 parents.
    
    All the tests currently pass, however the new test is not yet finished so
    I've left some TODO's in it for the meantime. The bottom section of
    commented out code should be correct as it checks the workspace contents
    after a revert to "right" - it just depends on the unimplemented revert to
    "right" code.
    
    Also, the code which checks the contents of the files should really be
    "left\nancestor", but I couldn't get it to work by using \n in the string.
    As a work around I check for both left + ancestor, and check for NOT right.

---
+++ test/func/revert_in_two_parent_workspace/__driver__.lua
@@ -0,0 +1,65 @@
+-- create a workspace with 2 parents then revert to just one of them
+-- and check the workspace is consistent with it
+
+mtn_setup()
+
+addfile("testfile", "ancestor\nancestor")
+addfile("otherfile", "blah blah")
+commit()
+anc = base_revision()
+
+writefile("testfile", "left\nancestor")
+writefile("otherfile", "modified too")
+commit()
+left = base_revision()
+
+revert_to(anc)
+writefile("testfile", "ancestor\nright")
+commit()
+right = base_revision()
+
+check(mtn("merge_into_workspace", left), 0, false, false)
+check(qgrep("left", "testfile"))
+check(qgrep("right", "testfile"))
+check(not qgrep("ancestor", "testfile"))
+
+-- we now have workspace with 2 parents, so lets try a revert
+-- without specifying a revision - mtn should throw an error
+
+check(mtn("revert", "."), 1, nil, true)
+need_to_specify_diag = "mtn: misuse: this workspace has multiple parents. specify which parent to use with -r\n"
+check(grep("-v", "detected at", "stderr"), 0, need_to_specify_diag)
+
+-- now lets try a revert by specifying an invalid parent revision.
+-- the revision id MUST be valid - it just needs to NOT be a parent of the
+-- current workspace. if the revision id is not valid, mtn will throw an error
+-- about "no match for selection" - which we don't care about in this test
+
+check(mtn("revert", ".", "-r", anc), 1, nil, true)
+incorrect_parent_diag = "mtn: misuse: the specified revision is not a parent of the current workspace\n"
+check(grep("-v", "detected at", "stderr"), 0, incorrect_parent_diag)
+
+-- now lets revert back to a proper revision - and check the workspace
+-- contents
+check(mtn("revert", ".", "-r", left), 0, nil, true)
+-- TODO: don't know how to check for left\nancestor
+check(qgrep("left", "testfile"))
+check(qgrep("ancestor", "testfile"))
+check(not qgrep("right", "testfile"))
+
+check(qgrep("modified too", "otherfile"))
+
+-- TODO: file contents are fine - now lets check if the book keeping is
+-- correct by running "mtn status" and "mtn ls changed" - there should
+-- be no changes
+
+
+
+-- TODO: perform revert to right as well
+-- TODO: check for ancestor\nancestor
+--check(qgrep("ancestor", "testfile"))
+--check(not qgrep("left", "testfile"))
+--check(not qgrep("right", "testfile"))
+
+--check(qgrep("blah blah", "otherfile"))
+
