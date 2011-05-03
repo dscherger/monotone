94ce1a74c5f5cc500ae6f6c047be1b3a842da168
Stephen Leake <stephen_leake@stephe-leake.org>
2011-05-03 10:12:59 +0000

    * test/func/alt_tmpdir: New test, for alternate tmpdir feature (just getting started)

---
+++ test/func/alt_tmpdir/__driver__.lua
@@ -0,0 +1,39 @@
+-- Test the alternate tmpdir capability.
+--
+-- not a very thorough test; on Windows, files can be moved between
+-- devices, and on Linux, it requires manual intervention to set up a
+-- workspace that spans two devices. So we just verify that the
+-- 'alt_tmpdir' keyword in _MTN/options is supported. The actual
+-- operation is tested by use in real workspaces.
+
+mtn_setup()
+
+addfile("readme", "just a file")
+
+mkdir("local")
+addfile("local/local-file0", "local file 0\n")
+
+mkdir("nfs_mounted")
+addfile("nfs_mounted/nfs_mounted-file0", "nfs_mounted file 0\n")
+
+commit()
+rev0 = base_revision()
+
+addfile("local/local-dir/file1", "local file 1\n")
+addfile("local/local-dir/file2", "local file 2\n")
+
+addfile("nfs_mounted/nfs_mounted-dir/file1", "nfs_mounted file 1\n")
+addfile("nfs_mounted/nfs_mounted-dir/file2", "nfs_mounted file 2\n")
+
+commit()
+rev1 = base_revision()
+
+revert_to(rev0)
+
+execute(["echo", "tmpdir \"nfs_mounted\" \"nfs_mounted/tmp\" > _MTN/options"]) 
+
+mkdir("nfs_mounted/tmp")
+
+check(mtn("update"), 0, nil, true)
+
+-- end of file
