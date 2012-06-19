878f8f4a37c27575cc406b6017ee03fe41918bc4
Stephen Leake <stephen_leake@stephe-leake.org>
2012-06-19 11:43:55 +0000

    * src/merge_content.cc (resolve_merge_conflicts): require --resolve_conflicts for all conflicts except dropped_modified. Still not quite right; don't get proper report of conflict from merge commands, just "no resolution specified"

--- src/merge_content.cc
+++ src/merge_content.cc
@@ -722,29 +722,38 @@ resolve_merge_conflicts(lua_hooks & lua,
     {
       result.log_conflicts();
 
-      // Apply any conflict resolutions. If the user did not specify
-      // --resolve_conflicts, there may still be some specified by attr
-      // mtn:resolve_conflict.
+      if (resolutions_given)
+        {
+          // If there are any conflicts for which we don't currently support
+          // resolutions, give a nice error message.
+          char const * const msg = "conflict resolution for %s not yet supported";
 
-      char const * const msg = "conflict resolution for %s not yet supported";
+          E(!result.missing_root_conflict, origin::user,
+            F(msg) % "missing_root_dir");
+          E(result.invalid_name_conflicts.size() == 0, origin::user,
+            F(msg) % "invalid_name_conflicts");
+          E(result.directory_loop_conflicts.size() == 0, origin::user,
+            F(msg) % "directory_loop_conflicts");
+          E(result.multiple_name_conflicts.size() == 0, origin::user,
+            F(msg) % "multiple_name_conflicts");
+          E(result.attribute_conflicts.size() == 0, origin::user,
+            F(msg) % "attribute_conflicts");
 
-      E(!result.missing_root_conflict, origin::user,
-        F(msg) % "missing_root_dir");
-      E(result.invalid_name_conflicts.size() == 0, origin::user,
-        F(msg) % "invalid_name_conflicts");
-      E(result.directory_loop_conflicts.size() == 0, origin::user,
-        F(msg) % "directory_loop_conflicts");
-      E(result.multiple_name_conflicts.size() == 0, origin::user,
-        F(msg) % "multiple_name_conflicts");
-      E(result.attribute_conflicts.size() == 0, origin::user,
-        F(msg) % "attribute_conflicts");
+          // Resolve the ones we can, if they have resolutions specified. Each
+          // conflict list is deleted once all are resolved.
+          result.resolve_orphaned_node_conflicts(lua, left_roster, right_roster, adaptor);
+          result.resolve_dropped_modified_conflicts(lua, left_roster, right_roster, adaptor, nis);
+          result.resolve_duplicate_name_conflicts(lua, left_roster, right_roster, adaptor);
 
-      // Resolve the ones we can, if they have resolutions specified. Each
-      // conflict list is deleted once all are resolved.
-      result.resolve_orphaned_node_conflicts(lua, left_roster, right_roster, adaptor);
-      result.resolve_dropped_modified_conflicts(lua, left_roster, right_roster, adaptor, nis);
-      result.resolve_duplicate_name_conflicts(lua, left_roster, right_roster, adaptor);
-      result.resolve_file_content_conflicts(lua, left_roster, right_roster, adaptor);
+          result.resolve_file_content_conflicts (lua, left_roster, right_roster, adaptor);
+        }
+      else
+        {
+          // The user did not specify --resolve_conflicts, but there may
+          // still be some specified by attr mtn:resolve_conflict. Only
+          // these conflicts support that so far.
+          result.resolve_dropped_modified_conflicts(lua, left_roster, right_roster, adaptor, nis);
+        }
     }
 
   if (result.has_non_content_conflicts())
