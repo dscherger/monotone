968f5069889ed71875259a417f8b138debc49df9
Stephen Leake <stephen_leake@stephe-leake.org>
2012-06-28 10:26:24 +0000

    * test/func/using_packets/__driver__.lua, doc/monotone.texi (Using packets): modified test to produce same file_ids, rev_ids as bash commands in example (forgot newlines)

--- test/func/using_packets/__driver__.lua
+++ test/func/using_packets/__driver__.lua
@@ -27,18 +27,18 @@ check(mtnA("setup", "--branch=test", "."), 0, false, false)
 
 -- add revisions to A
 
-writefile("file", "xyz")
+writefile("file", "xyz\n")
 check(mtnA("add", "file"), 0, nil, false)
 check(mtnA("commit", "-m", "One"), 0, nil, false)
 rev_one = base_revision()
 
-writefile("file2", "file 2 getting in")
-writefile("file", "ERASE")
+writefile("file2", "file 2 getting in\n")
+writefile("file", "ERASE\n")
 check(mtnA("add", "file2"), 0, nil, false)
 check(mtnA("commit", "-m", "Two"), 0, nil, false)
 rev_two = base_revision()
 
-writefile("file", "THIRD")
+writefile("file", "THIRD\n")
 check(mtnA("commit", "-m", "Three"), 0, nil, false)
 rev_three = base_revision()
 
@@ -48,13 +48,13 @@ check(mtnA("automate", "select", "i:"), 0, true, nil)
 check(samelines("stdout", {rev_three, rev_one, rev_two}))
 -- show the rev ids, so we can copy them into monotone.texi
 check(samelines("stdout",
-{"299b021a6d426f2ff3b09fabcd165abe5d82f2b5", -- rev_three
- "2d0b9a10f73ce2445213506c2a0a81b8e910b923", -- rev_one
- "b2ae639e410e4970cab274a580e87d08adc87ee7"})) -- rev_two
+{"151f1fb125f19ebe11eb8bfe3a5798fcbea4e736", -- rev_three
+ "a423db0ad651c74e41ab2529eca6f17513ccf714", -- rev_one
+ "d14e89582ad9030e1eb62f563c8721be02ca0b65"})) -- rev_two
 
 -- Transfer rev_one to db B
 -- first, get the file id
-file_id_one = "66b27417d37e024c46526c2f6d358a754fc552f3"
+file_id_one = "8714e0ef31edb00e33683f575274379955b3526c"
 check(mtnA("automate", "get_revision", rev_one), 0, true, nil)
 check(qgrep(file_id_one, "stdout"))
 
@@ -76,8 +76,8 @@ rename("stdout", "PACKET3")
 check(mtnB("read", "KEY_PACKETS", "PACKET1", "PACKET2", "PACKET3"), 0, nil, false)
 
 -- Now transfer rev_two
-file_id_two = "491cd4818acc21cb6fb3f8fd5676633d60a6b973"
-file2_id_two = "ca085d538b534aa2f1746ac9a61c6f1d279356a4"
+file_id_two = "8b52d96d4fab6c1e56d6364b0a2673f4111b228e"
+file2_id_two = "d2178687226560032947c1deacb39d16a16ea5c6"
 check(mtnA("automate", "get_revision", rev_two), 0, true, nil)
 check(qgrep(file_id_two, "stdout"))
 check(qgrep(file2_id_two, "stdout"))
